6d88a57b57f782f2121d8cd5bbbb42926576e584
Darrick Truong <truong.darrick@gmail.com>
2020-10-16 10:55:05 -0700

    changed classes

--- src/employeedepartment/Department.java
+++ src/employeedepartment/Department.java
@@ -1,299 +1,82 @@
 package employeedepartment;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
-public class Department implements Comparable<Department>{
+public class Department {
 	
-	private static ArrayList<String> nameList = new ArrayList<String>();
-	private static ArrayList<OldEmployee> managerList = new ArrayList<OldEmployee>();
 	private String name;
 	private String address;
 	private String phone;
 	private int budget;
-	private OldEmployee manager;
+	private int deptManager;
 	
-	public Department(String name, String address, String phone, int budget, OldEmployee manager) {
+	
+	public Department(String name, String address, String phone, int budget, int deptManager) {
 		super();
-		Scanner sc = new Scanner(System.in);
-		name = name.toUpperCase();
-		
-		if (!nameList.contains(name)) {
-			this.name = name;
-			nameList.add(name);
-		} else {
-			while(nameList.contains(name)) {
-				System.out.println("Department name already exists, please enter a unique name.");
-				name = sc.nextLine();
-			}
-			this.name = name;
-			nameList.add(name);
-		}
-		
 		this.name = name;
-		
-		Pattern p = Pattern.compile("[0-9]* ([A-Z]?[a-z]*\\s*){1,3} ([a-z]){2}\\. [A-Z]{2} [0-9]{5}");
-		Matcher m = p.matcher(address);
-		
-		while(!m.matches()) {
-			
-			System.out.println("Invalid address. Please enter a new one");
-			address = sc.nextLine();
-			m = p.matcher(address);
-		}
-		
 		this.address = address;
-		
-		Pattern p2 = Pattern.compile("\\d{10}");
-		Matcher m2 = p2.matcher(phone);
-		
-		while(!m2.matches()) {
-			
-			System.out.println("Invalid phone number. Please enter a new one");
-			phone = sc.nextLine();
-			m = p.matcher(phone);
-		}
-		
 		this.phone = phone;
 		this.budget = budget;
-		
-		try {
-			if (manager != null) {
-				if (!manager.isActive()) {
-					throw new ActiveManagerException(manager);
-				}
-				if (!managerList.contains(manager)) {
-					this.manager = manager;
-					this.manager.setDepartment(this);
-					managerList.add(this.manager);
-					this.manager.setRole("manager");
-				}
-			
-			} else {
-				this.manager = null;			
-			}		
-		} catch (ActiveManagerException e){
-			System.out.println(e.getMessage());
-		} 
+		this.deptManager = deptManager;
 	}
+
 
 	public String getName() {
 		return name;
 	}
 
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 
 	public String getAddress() {
 		return address;
 	}
 
+
 	public void setAddress(String address) {
-		Scanner sc = new Scanner(System.in);
-		Pattern p = Pattern.compile("[0-9]* ([A-Z]?[a-z]*\\s*){1,3} ([a-z]){2}\\. [A-Z]{2} [0-9]{5}");
-		Matcher m = p.matcher(address);
-		
-		while(!m.matches()) {	
-			
-			System.out.println("Invalid address. Please enter a new one");
-			address = sc.nextLine();
-			m = p.matcher(address);
-		}
-		
 		this.address = address;
 	}
+
 
 	public String getPhone() {
 		return phone;
 	}
 
+
 	public void setPhone(String phone) {
-		Scanner sc = new Scanner(System.in);
-		Pattern p = Pattern.compile("\\d{10}");
-		Matcher m = p.matcher(phone);
-		
-		while(!m.matches()) {
-			
-			System.out.println("Invalid phone number. Please enter a new one");
-			phone = sc.nextLine();
-			m = p.matcher(phone);
-		}
 		this.phone = phone;
-		
 	}
+
 
 	public int getBudget() {
 		return budget;
 	}
 
+
 	public void setBudget(int budget) {
 		this.budget = budget;
 	}
 
-	public OldEmployee getManager() {
-		return manager;
-	}
-
-	public void setManager(OldEmployee manager) {
-		
-		
-		if (manager == null) {
-			this.manager = null;
-			return;
-		}
-		
-		try {
-			if (!manager.isActive()) {
-				throw new ActiveManagerException(manager);
-			}
-			if (!managerList.contains(manager)) {
-				this.manager = manager;
-				this.manager.setDepartment(this);
-				managerList.add(this.manager);
-			} else {
-				this.manager = null;
-				System.out.println("Employee can only be manager for one department"); // custom exception
-			}
-		} catch (ActiveManagerException e){
-			System.out.println(e.getMessage());
-		}
-	}
-	
-	
-	public int compareTo(Department department) {
-		
-		return this.name.compareTo(department.getName());
-	}
 
 	
-	public static void departmentList(ArrayList<Department> list) {
-		Stream<Department> depStream = list.parallelStream();
-		depStream.forEach(System.out::println);
+
+	public int getDeptManager() {
+		return deptManager;
 	}
-	
-	public static Department getDepartment(ArrayList<Department> depList, String name) {
-		name = name.toUpperCase();
-		if (!nameList.contains(name)) {
-			return null;
-		}
-		Department d1 = null;
-		for (Department dep: depList) {
-			if (dep.getName().equals(name)) {
-				return dep;
-			}
-		}
-		return d1;
+
+
+	public void setDeptManager(int deptManager) {
+		this.deptManager = deptManager;
 	}
-	
-	public static void updateDepartment(Department department, String name, String address, String phone, int budget, OldEmployee manager) {
-		Scanner sc = new Scanner(System.in);
-		nameList.remove(department.getName());
-		
-		
-		if (!nameList.contains(name)) {
-			department.setName(name.toUpperCase());
-			nameList.add(department.name);
-		} else {
-			while(nameList.contains(name.toUpperCase())) {
-				System.out.println("Department name already exists, please enter a unique name.");
-				name = sc.nextLine();
-			}
-			department.setName(name.toUpperCase());
-			nameList.add(department.name);
-		}
-		
-		
-//		Pattern p = Pattern.compile("[0-9]* ([A-Z]?[a-z]*\\s*){1,3} ([a-z]){2}\\. [A-Z]{2} [0-9]{5}");
-//		Matcher m = p.matcher(address);
-//		
-//		while(!m.matches()) {
-//			
-//			System.out.println("Invalid address. Please enter a new one");
-//			address = EmployeeDepartmentDriver.sc.nextLine();
-//			m = p.matcher(address);
-//		}
-		
-		department.setAddress(address);
-		
-//		Pattern p2 = Pattern.compile("\\d{10}");
-//		Matcher m2 = p2.matcher(phone);
-//		
-//		while(!m2.matches()) {
-//			
-//			System.out.println("Invalid phone number. Please enter a new one");
-//			phone = EmployeeDepartmentDriver.sc.nextLine();
-//			
-//		}
-		
-		department.setPhone(phone);
-		department.setBudget(budget);
-		
-		if (manager != null) {
-			try {
-				if (!manager.isActive()) {
-					throw new ActiveManagerException(manager);
-				}
-				department.setManager(manager);
-				manager.setDepartment(department);
-				manager.setRole("manager");
-				
-			} catch (ActiveManagerException e){
-				System.out.println(e.getMessage());
-			}
-		}
-		
-	}
-	
-	public static void removeDepartment(Department department) {
-		
-		nameList.remove(department.getName());
-		
-		if (department.getManager() != null) {
-			department.getManager().setRole("reassign");
-		}
-		
-		
-	}
-	
-	
-//	private void changeManager() {
-//		
-//		boolean replace = true;
-//		do {
-//			
-//		} while(replace);
-//	}
-	
-	public String toFileStringDepartment() {
-		if (manager != null) {
-			return "" + name +"&"+ address +"&"+ phone + "&" + budget +"&"+ manager.getId();
-		} else {
-			return "" + name +"&"+ address +"&"+ phone + "&" + budget +"&"+ "null";
-		}
-		
-	}
-	
-	
-	
 	
 	@Override
 	public String toString() {
-		if (manager != null)  {
-			return "Department [name=" + name + ", address=" + address + ", phone=" + phone + ", budget=" + budget
-					+ ", manager=" + manager.getName() + "]";
-		} else {
-			return "Department [name=" + name + ", address=" + address + ", phone=" + phone + ", budget=" + budget
-					+ ", manager= no manager]";
-		}
-		
+		return "Department [name=" + name + ", address=" + address + ", phone=" + phone + ", budget=" + budget + "]";
 	}
-	
-	
-	
-	
 
+	
+	
+	
 }
--- src/employeedepartment/Employee.java
+++ src/employeedepartment/Employee.java
@@ -8,9 +8,70 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
-public class Employee implements Comparable<Employee>{
+public class Employee {
 	
-	
+		private String firstName;
+		private String lastName;
+		private String phone;
+		private String title;
+		
+		public Employee(String firstName, String lastName, String phone, String title, int salary, int deptId) {
+			super();
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.phone = phone;
+			this.title = title;
+			this.salary = salary;
+			this.deptId = deptId;
+		}
+		
+		private int salary;
+		private int deptId;
+
+		public String getFirstName() {
+			return firstName;
+		}
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+		public String getLastName() {
+			return lastName;
+		}
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+		public String getPhone() {
+			return phone;
+		}
+		public void setPhone(String phone) {
+			this.phone = phone;
+		}
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		public int getSalary() {
+			return salary;
+		}
+		public void setSalary(int salary) {
+			this.salary = salary;
+		}
+		public int getDeptId() {
+			return deptId;
+		}
+		public void setDeptId(int deptId) {
+			this.deptId = deptId;
+		}
+		
+		@Override
+		public String toString() {
+			return "Employee [firstName=" + firstName + ", lastName=" + lastName + ", phone=" + phone + ", title="
+					+ title + ", salary=" + salary + ", deptId=" + deptId + "]";
+		}
+		
+		
 		
 	}
 	
@@ -20,4 +81,4 @@ public class Employee implements Comparable<Employee>{
 	
 	
 	
-}
+
